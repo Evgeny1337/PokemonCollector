f66c865dd3c327c867ff62e1bbc6602d0afe553b
evgeny1337 <genya-sozykin@mail.ru>
2024-12-15 18:09:10 +0500

    Removed unnecessary fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,8 +4,8 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     id = models.AutoField(
         auto_created=True, primary_key=True, verbose_name="Идентификатор")
-    title = models.CharField(
-        max_length=400, verbose_name='Наименование на русском')
+    title = models.CharField(null=True,
+                             max_length=400, verbose_name='Наименование на русском')
     image = models.ImageField(null=True, verbose_name='Изображение')
     description = models.TextField(null=True, verbose_name="Описание")
     title_en = models.CharField(
@@ -30,8 +30,3 @@ class PokemonEntity(models.Model):
         verbose_name='Время появления', null=True)
     disappeared_at = models.DateTimeField(
         verbose_name='Время исчезновения', null=True)
-    level = models.IntegerField(verbose_name='Уровень')
-    health = models.IntegerField(verbose_name='Здоровье')
-    strength = models.IntegerField(verbose_name='Сила')
-    defence = models.IntegerField(verbose_name='Броня')
-    stamina = models.IntegerField(verbose_name='Выносливость')
