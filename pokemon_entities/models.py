8dc85db8bec7cc2a7168ae842342fa5507471ebe
evgeny1337 <genya-sozykin@mail.ru>
2025-01-09 22:22:50 +0500

    fix related_name

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,14 +4,15 @@ from django.db import models
 class Pokemon(models.Model):
     title = models.CharField(null=True,
                              max_length=400, verbose_name='Наименование на русском')
-    image = models.ImageField(null=True,blank=True, verbose_name='Изображение')
+    image = models.ImageField(null=True, blank=True,
+                              verbose_name='Изображение')
     description = models.TextField(null=True, verbose_name="Описание")
     title_en = models.CharField(
         max_length=400, null=True, verbose_name='Наименование на английском')
     title_jp = models.CharField(
         max_length=400, null=True, verbose_name='Наименование на японском')
     parent = models.ForeignKey(
-        'self',null=True, blank=True, 
+        'self', null=True, blank=True,
         verbose_name='Предок покемона',
         related_name='children',
         on_delete=models.CASCADE)
@@ -23,12 +24,13 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     latitude = models.FloatField(verbose_name='Широта')
     longitude = models.FloatField(verbose_name='Долгота')
-    pokemon = models.ForeignKey(Pokemon,related_name='pokemons',on_delete=models.CASCADE, null=True)
+    pokemon = models.ForeignKey(
+        Pokemon, on_delete=models.CASCADE, null=True)
     appeared_at = models.DateTimeField(
         verbose_name='Время появления', null=True)
     disappeared_at = models.DateTimeField(
         verbose_name='Время исчезновения', null=True)
-    
+
     def __str__(self):
         pokemon_name = self.pokemon.title
         latitude = self.latitude
